057609fca300c735eb5f1d1830f341121e349cd7
John Lynch <johnlynch.32768@gmail.com>
2019-08-15 21:25:39 +0100

    Split frequency data into 4 canvases

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 document.addEventListener('click', function() {
-    context.resume().then(() => {
+    audio_context.resume().then(() => {
         console.log('Playback resumed successfully');
     });
 });
 
 let audio = new Audio();
-audio.src = 'track7.mp3';
+audio.src = 'track1.mp3';
 audio.controls = true;
 audio.loop = true;
 audio.autoplay = true;
@@ -13,37 +13,40 @@ let frame = 0;
 const bar_width = 4;
 const bars = 256;
 
-let canvas, ctx, source, context, analyser, fbc_array, bar_height, bar_x;
+let source, audio_context, analyser, spectrum, bar_height, bar_x;
+let canvases = [0, 1, 2, 3].map(n => document.getElementById(`canvas-${n}`));
+let graphics_contexts = canvases.map(canvas => canvas.getContext('2d'));
+let [w, h] = [canvases[0].width, canvases[0].height];
+
 
 window.addEventListener("load", initPlayer, false);
 
 function initPlayer(){
     document.getElementById('audio_box').appendChild(audio);
-    context = new AudioContext();
-    analyser = context.createAnalyser(); 
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+    audio_context = new AudioContext();
+    analyser = audio_context.createAnalyser(); 
     // Re-route audio playback into the processing graph of the AudioContext
-    source = context.createMediaElementSource(audio); 
+    source = audio_context.createMediaElementSource(audio); 
     source.connect(analyser);
-    analyser.connect(context.destination);
+    analyser.connect(audio_context.destination);
     paintBars();
 }
 
 function paintBars(){
+    frame++;
     requestAnimationFrame(paintBars);
     spectrum = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(spectrum);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // ctx.fillStyle = '#ff0044';
-    frame++;
-    for (let i = 0; i < bars; i++) {
-        bar_x = i * 5;
-        let val = spectrum[i * 4];
-        ctx.fillStyle = `rgb(${val}, ${((frame / 31) + (frame % (i + 173)) * i) % 256}, ${255 - val})`;
-        bar_height = -val * 0.6;
-        ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
-    }
+    graphics_contexts.forEach((ctx, n) => {
+        ctx.clearRect(0, 0, w, h);
+        for (let i = 0; i < bars; i++) {
+            bar_x = i * 5;
+            let val = spectrum[n * bars + i];
+            ctx.fillStyle = `rgb(${val}, ${((frame / 31) + (frame % (i + 173)) * i) % 256}, ${255 - val})`;
+            bar_height = -val * 0.6 * (n + 1);
+            ctx.fillRect(bar_x, h, bar_width, bar_height);
+        }
+    });
 }
 
 // function rgb2Hex(rgbColour) {
